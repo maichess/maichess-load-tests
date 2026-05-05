b7daccc66da99b9223146356e4c53f52eed7a355
Tom Berger <berger.tom@mail.de>
2026-05-05 10:22:22 +0200

    staging urls

--- src/test/scala/config/ServiceConfig.scala
+++ src/test/scala/config/ServiceConfig.scala
@@ -8,11 +8,11 @@ object ServiceConfig:
   private def prop(key: String, fallback: String): String =
     sys.props.getOrElse(key, sys.env.getOrElse(key.toUpperCase.replace('.', '_'), fallback))
 
-  val authUrl         = prop("auth.url",          "https://auth.maichess.berger-software.com")
-  val userUrl         = prop("user.url",          "https://users.maichess.berger-software.com")
-  val matchMakerUrl   = prop("matchmaker.url",    "https://matchmaker.maichess.berger-software.com")
-  val matchManagerUrl = prop("matchmanager.url",  "https://matchmanager.maichess.berger-software.com")
-  val analysisUrl     = prop("analysis.url",      "https://analysis.maichess.berger-software.com")
+  val authUrl         = prop("auth.url",          "https://staging.auth.maichess.berger-software.com")
+  val userUrl         = prop("user.url",          "https://staging.users.maichess.berger-software.com")
+  val matchMakerUrl   = prop("matchmaker.url",    "https://staging.matchmaker.maichess.berger-software.com")
+  val matchManagerUrl = prop("matchmanager.url",  "https://staging.matchmanager.maichess.berger-software.com")
+  val analysisUrl     = prop("analysis.url",      "https://staging.analysis.maichess.berger-software.com")
 
   val baseProtocol =
     http.acceptHeader("application/json")
